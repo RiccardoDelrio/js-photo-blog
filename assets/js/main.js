fda797b6b1664683c4b7fcd0fdfd2c74e5043e33
Riccardo Delrio <riccardodelrio92@gmail.com>
2025-02-28 18:30:37 +0100

    fetch and display images from API, update main styles and structure

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const cardContainerEl = document.querySelector('.card_container');
+fetch('https://lanciweb.github.io/demo/api/pictures/')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            const { date, title, url } = element;
+            console.log(date, title, url); 
+            cardContainerEl.insertAdjacentHTML('beforeend', 
+                `<div class="col-12 col-md-6 col-lg-4 mt-4 ">
+                    <div class="card">
+                        <img src="./assets/img/pin.svg" alt="" class="pin">
+                        <img src="${url}" class="card-img-top img_p p-3" alt="...">
+                        <div class="card-body">
+                            <p class="card-text date">${date}</p>
+                            <p class="card-text photo_title">${title}</p>
+                        </div>
+                    </div>
+                </div>`
+            );
+            
+        });
+    })
